cbc8df2b23a19d2102e4721612ac1271ed0acbeb
Reese <reese.westerhoff@gmail.com>
2026-04-13 15:05:46 +0100

    add vitest config file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,12 @@
-import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   test: {
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
-    globals: true
+    globals: true,
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, ".")
-    }
-  }
 });
